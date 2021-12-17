41f442158b8d15d5507fbddb54697531f62877b0
radityohanif <sayaanakit80@gmail.com>
2021-12-17 17:04:16 +0700

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Pada kode di atas Anda telah membuat inisialisasi LINE bot dan SLIM App. Selanjutnya buat route untuk URL Webhook ke public/webhook dengan method POST. Setiap request yang masuk akan dicek terlebih dahulu oleh header content X-Line-Signature apakah valid atau tidak. Kemudian jika lolos validasi signature ini, kode implementasi fitur-fitur Messaging API nantinya akan berjalan.
+ * Kita akan membuat inisialisasi LINE bot dan SLIM App. Selanjutnya buat route untuk URL Webhook ke public/webhook dengan method POST. Setiap request yang masuk akan dicek terlebih dahulu oleh header content X-Line-Signature apakah valid atau tidak. Kemudian jika lolos validasi signature ini, kode implementasi fitur-fitur Messaging API nantinya akan berjalan.
  
  * Catatan: Bila hendak mencoba simulasi request, misalnya melalui aplikasi Postman, Anda dapat atur variabel $pass_signature = true; untuk melewati proses pengecekan signature.
  */
@@ -20,7 +20,7 @@ use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 use LINE\LINEBot\SignatureValidator as SignatureValidator;
 
 $pass_signature = true;
-$channel_access_token = "";
+$channel_access_token = "fRIKZ6iNY3CgQnmbEXlh/MN4J4ZOlABuuH9HrYlb+XHj13iubIwiV7GdUceE+/hPQycNn9o4vNG0BVu75Io8BW3ECOzrWeJX8tICgcAkuHSF0SU4YYPj+dRUVPdvyTaSoRxhizvrZuJVCp6vsDGpoAdB04t89/1O/w1cDnyilFU=";
 $channel_secret = "c910dfb912797f796ce793d91c42da7b";
 
 $httpClient = new CurlHTTPClient($channel_access_token);
@@ -29,14 +29,14 @@ $bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
 $app = AppFactory::create();
 $app->setBasePath("/public");
 
-$app->get('/', function(Request $request, Response $response, $args){
+$app->get('/', function (Request $request, Response $response, $args) {
   $response->getBody()->write("Hello World");
   return $response;
 });
 
 // buat root untuk webhook
-$app->post('/webhook', function(Request $request, Response $response) use($channel_secret, $bot, $httpClient, $pass_signature) {
-  
+$app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
+
   // get request body and line signature header
   $body = $request->getBody();
   $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
@@ -44,15 +44,15 @@ $app->post('/webhook', function(Request $request, Response $response) use($chann
   // log body and signature
   file_put_contents('php://stderr', 'Body: ' . $body);
 
-  if($pass_signature === false) {
-    
+  if ($pass_signature === false) {
+
     // if LINE_SIGNATURE exist in request header
-    if(empty($signature)) {
+    if (empty($signature)) {
       return $response->withStatus(400, 'Signature not set');
     }
 
     // is this request comes from LINE?
-    if(!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
+    if (!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
       return $response->withStatus(400, 'Invalid Signature');
     }
   }
