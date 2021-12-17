d9d1ba06c330acf83887001ce64e9abe66d6e369
radityohanif <sayaanakit80@gmail.com>
2021-12-17 16:41:37 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Pada kode di atas Anda telah membuat inisialisasi LINE bot dan SLIM App. Selanjutnya buat route untuk URL Webhook ke public/webhook dengan method POST. Setiap request yang masuk akan dicek terlebih dahulu oleh header content X-Line-Signature apakah valid atau tidak. Kemudian jika lolos validasi signature ini, kode implementasi fitur-fitur Messaging API nantinya akan berjalan.
+ 
+ * Catatan: Bila hendak mencoba simulasi request, misalnya melalui aplikasi Postman, Anda dapat atur variabel $pass_signature = true; untuk melewati proses pengecekan signature.
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\RequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\SignatureValidator as SignatureValidator;
+
+$pass_signature = true;
+$channel_access_token = "";
+$channel_secret = "c910dfb912797f796ce793d91c42da7b";
+
+$httpClient = new CurlHTTPClient($channel_access_token);
+$bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+
+$app = AppFactory::create();
+$app->setBasePath("/public");
+
+$app->get('/', function(Request $request, Response $response, $args){
+  $response->getBody()->write("Hello World");
+  return $response;
+});
+
+// buat root untuk webhook
+$app->post('/webhook', function(Request $request, Response $response) use($channel_secret, $bot, $httpClient, $pass_signature) {
+  
+  // get request body and line signature header
+  $body = $request->getBody();
+  $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
+
+  // log body and signature
+  file_put_contents('php://stderr', 'Body: ' . $body);
+
+  if($pass_signature === false) {
+    
+    // if LINE_SIGNATURE exist in request header
+    if(empty($signature)) {
+      return $response->withStatus(400, 'Signature not set');
+    }
+
+    // is this request comes from LINE?
+    if(!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
+      return $response->withStatus(400, 'Invalid Signature');
+    }
+  }
+
+  // Kode aplikasi nanti disini..
+
+});
+
+$app->run();
